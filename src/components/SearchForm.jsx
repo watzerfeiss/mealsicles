fa714a3cad8b6b8b54620a1f57a0fbd9ffdfda15
ppesterev <ppesterev@mail.ru>
2020-11-29 04:40:31 +0700

    search form styles, turned out alright actually

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -38,6 +38,7 @@ export default function SearchForm({
     >
       {isOpen && (
         <input
+          className="search-form__field"
           type="search"
           ref={searchField}
           value={searchText}
@@ -46,20 +47,67 @@ export default function SearchForm({
       )}
       {isOpen ? (
         <>
-          <input type="submit" value="Search" />
+          <button className="btn search-form__btn" type="submit" value="Search">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
           {compact && (
             <button
+              className="btn search-form__btn"
               type="button"
               onClick={() => setSearching(false)}
               value="Cancel"
             >
-              x
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
             </button>
           )}
         </>
       ) : (
-        <button type="button" onClick={() => setSearching(true)} value="Search">
-          p
+        <button
+          className="btn search-form__btn"
+          type="button"
+          onClick={() => setSearching(true)}
+          value="Search"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
         </button>
       )}
     </form>
