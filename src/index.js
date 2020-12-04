2e9c84e48d429e4784b81c8a5bd865612288f785
ppesterev <ppesterev@mail.ru>
2020-12-05 00:10:59 +0700

    add ScrollToTop

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 
 import App from "./components/App";
+import ScrollToTop from "./components/ScrollToTop";
 
 import "./styles/styles.css";
 
 ReactDOM.render(
   <HashRouter>
     <App />
+    <ScrollToTop />
   </HashRouter>,
   document.getElementById("app")
 );
