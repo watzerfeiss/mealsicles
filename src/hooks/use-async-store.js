341d01edb27ec693346e27706e27fe0816fe57c2
ppesterev <ppesterev@mail.ru>
2021-04-11 22:25:48 +0700

    fix state update logic in async store

--- src/hooks/use-async-store.js
+++ src/hooks/use-async-store.js
@@ -9,13 +9,15 @@ function withThunks(dispatch, getState) {
   };
 }
 
-export default function useAsyncStore(...args) {
+export default function useAsyncStore(reducer, ...args) {
   const stateRef = useRef(null);
-  const [state, dispatch] = useReducer(...args);
+  const [state, dispatch] = useReducer((state, action) => {
+    stateRef.current = reducer(state, action);
+    return stateRef.current;
+  }, ...args);
 
-  stateRef.current = state;
   const getState = useCallback(() => stateRef.current, [stateRef]);
 
   const asyncDispatch = useMemo(() => withThunks(dispatch, getState), []);
-  return [stateRef.current, asyncDispatch];
+  return [state, asyncDispatch];
 }
