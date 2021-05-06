5f8b8da1a9cf8c1ab576904ea0ff7801ce16991a
ppesterev <ppesterev@mail.ru>
2021-05-06 18:26:36 +0700

    modal close button positioning, close on escape

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,24 +7,38 @@ import PropTypes from "prop-types";
 function Modal({ onClose, children }) {
   const { modalContainerRef, openModal, closeModal } = useContext(ModalContext);
 
+  const handleClose = () => {
+    onClose();
+    closeModal();
+  };
+
+  const closeOnEscape = (evt) => {
+    if (evt.key === "Escape") {
+      handleClose();
+    }
+  };
+
   useEffect(() => {
     openModal();
-    return () => closeModal();
+    document.addEventListener("keydown", closeOnEscape);
+    return () => {
+      document.removeEventListener("keydown", closeOnEscape);
+      handleClose();
+    };
   }, []);
 
   const modalElement = (
     <div className="modal">
-      <button
-        className="modal__close-btn"
-        type="button"
-        value="Close"
-        onClick={() => {
-          onClose();
-          closeModal();
-        }}
-      >
-        Close
-      </button>
+      <div className="modal__close-btn-wrapper">
+        <button
+          className="btn modal__close-btn"
+          type="button"
+          value="Close"
+          onClick={handleClose}
+        >
+          Close
+        </button>
+      </div>
       {children}
     </div>
   );
