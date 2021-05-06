3183d1bde2cb263f64ded0f6e60b310256a04d1b
ppesterev <ppesterev@mail.ru>
2021-05-06 20:39:24 +0700

    align motd badge in fave card, add hidden text description

--- src/components/MotdBadge.jsx
+++ src/components/MotdBadge.jsx
@@ -21,6 +21,7 @@ function MotdBadge(props) {
           d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
         />
       </svg>
+      <span className="visually-hidden">Meal of the day!</span>
     </span>
   );
 }
