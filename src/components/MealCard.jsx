91b310e73d0108746614d51806cfe48af8bc5748
ppesterev <ppesterev@mail.ru>
2021-05-07 15:09:26 +0700

    mealcard tweaks to prevent oversized images

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -10,14 +10,16 @@ import MotdBadge from "./MotdBadge";
 export default function MealCard({ meal, isMotd = false }) {
   return (
     <article className="meal-card">
-      <h3 className="meal-card__title">{meal.name}</h3>
-      {isMotd && <MotdBadge className="meal-card__motd-badge" />}
+      <header className="meal-card__header">
+        <h3 className="meal-card__title">{meal.name}</h3>
+        {isMotd && <MotdBadge className="meal-card__motd-badge" />}
+      </header>
       <img
         className="meal-card__image"
         src={meal.thumbnail}
         alt={meal.name}
-        width="10"
-        height="10"
+        width="100"
+        height="100"
       />
       <Link
         to={`/meal-details/${meal.id}`}
