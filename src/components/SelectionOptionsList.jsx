38a566552c9fcce955bdd9ec7a7f09675ba1a679
ppesterev <ppesterev@mail.ru>
2021-11-27 23:28:07 +0700

    remove all extracted functionality from selection list

--- src/components/SelectionOptionsList.jsx
+++ src/components/SelectionOptionsList.jsx
@@ -1,116 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
-import { loadSelectionOptions } from "../store/actions";
 import * as shapes from "../shapes";
 
-import CategoryCard from "./CategoryCard";
+import CategoryList from "./CategoryList";
+import AreaList from "./AreaList";
 import IngredientList from "./IngredientList";
-import Modal from "./Modal";
 
-export default function SelectionOptionsList({
-  dispatch,
-  type,
-  selectionTypes,
-  isPreview = false,
-  numItems = 3
-}) {
-  const [expandedCategory, setExpandedCategory] = useState(null);
-
-  useEffect(() => {
-    if (!selectionTypes || !selectionTypes[type]) {
-      dispatch(loadSelectionOptions(type));
-    }
-  }, [selectionTypes, type]);
-
-  const options = selectionTypes ? selectionTypes[type] : null;
-
-  if (options && type === "ingredients") {
-    return (
-      <div className="ingredients">
-        <IngredientList ingredients={options} />
-      </div>
-    );
+export default function SelectionOptionsList({ type, options }) {
+  if (!options || !type) {
+    return null;
   }
 
-  //todo shuffle
-  const numItemsToShow = isPreview ? numItems : options?.length;
-  return (
-    <div className={`${type}${isPreview ? ` ${type}--preview` : ""}`}>
-      {options && (
-        <ul className={`${type}-list`}>
-          {options.slice(0, numItemsToShow).map((option) => {
-            let item = null;
-            switch (type) {
-              case "categories":
-                item = (
-                  <a
-                    className="categories-list__card-link"
-                    href="#"
-                    onClick={(evt) => {
-                      evt.preventDefault();
-                      setExpandedCategory(option);
-                    }}
-                  >
-                    <CategoryCard category={option} />
-                  </a>
-                );
-                break;
-              case "areas":
-                item = (
-                  <Link to={`/area/${option.name}`} className="area-link">
-                    {option.name}
-                  </Link>
-                );
-                break;
-            }
-
-            return (
-              <li
-                className={`${type}-list__item`}
-                key={option.id || option.name}
-              >
-                {item}
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      {isPreview && (
-        <Link to={type} className="page-link all-options-link">
-          See all {type}
-        </Link>
-      )}
-      {expandedCategory && (
-        <Modal onClose={() => setExpandedCategory(null)}>
-          <CategoryCard category={expandedCategory} isExpanded />
-        </Modal>
-      )}
-    </div>
-  );
+  switch (type) {
+    case "categories":
+      return <CategoryList categories={options} />;
+    case "areas":
+      return <AreaList areas={options} />;
+    case "ingredients":
+      return <IngredientList ingredients={options} />;
+    default:
+      return null;
+  }
 }
 
-// this component is something else. Not sure whether in a bad or good way.
-
 SelectionOptionsList.propTypes = {
-  dispatch: PropTypes.func.isRequired,
   type: PropTypes.string.isRequired,
-  selectionTypes: PropTypes.shape({
-    categories: PropTypes.arrayOf(shapes.category),
-    areas: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string
-      })
-    ),
-    ingredients: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string
-      })
-    )
-  }),
-  numItems: PropTypes.number,
-  isPreview: PropTypes.bool
+  options: PropTypes.oneOfType([
+    PropTypes.arrayOf(shapes.category),
+    PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string.isRequired }))
+  ]).isRequired
 };
